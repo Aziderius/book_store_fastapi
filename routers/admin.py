95e4cf23633c9379c30410eee974b6b917f2f3a4
Aziderius <143766082+Aziderius@users.noreply.github.com>
2024-02-01 15:04:00 -0700

    se agregaron todas las funcionalidades de admin y de users, ademas de una imagen de cardinalidad del proyecto

--- routers/admin.py
+++ routers/admin.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from models import Users, Own_library, Genre, Books, Author
+from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
-from database import SessionLocal
-
+from sqlalchemy.sql.expression import func, select
+from sqlalchemy import join
+from starlette import status
+from .auth import get_current_user
+from pydantic import BaseModel, Field
 
 router = APIRouter(
     prefix='/admin',
@@ -18,8 +23,127 @@ def get_db():
         db.close()
 
 
-db_dependency = Annotated[Session, Depends(get_db)]
+class NewBookRequest(BaseModel):
+    title: str = Field(min_length=2)
+    author_id: int = Field(gt=0)
+    genre_id: int = Field(gt=0)
+    published_date: int = Field(gt=0)
+    page_number: int = Field(gt=0)
+    price: float = Field(gt=0)
+    rating: int = Field(gt=0, lt=6)
+    synopsis: str = Field(min_length=2)
 
-@router.get("/")
-async def get_user():
-    return {'admin': 'authenticated'}
+
+class NewAuthorRequest(BaseModel):
+    author_name: str = Field(min_length=2)
+
+
+class NewGenreRequest(BaseModel):
+    genre_name: str = Field(min_length=2)
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_all_users(user: user_dependency, db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    return db.query(Users).all()
+
+
+@router.get("/user/{user_id}", status_code=status.HTTP_200_OK)
+async def get_user_by_id(user: user_dependency, db: db_dependency, user_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    if user_model is not None:
+        return user_model
+    raise HTTPException(status_code=404, detail='User not found')
+    
+
+@router.get("/libraries", status_code=status.HTTP_200_OK)
+async def get_all_libraries(user:user_dependency, db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    return db.query(Own_library).all()
+
+
+@router.get("/library/{library_id}", status_code=status.HTTP_200_OK)
+async def get_library_by_owner_id(user: user_dependency, db: db_dependency, library_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    library_model = db.query(Own_library).filter(Own_library.owner_id == library_id).all()
+    if library_model is not None:
+        return library_model
+    
+    raise HTTPException(status_code=404, detail='User not found')
+
+
+@router.post("/add_genre", status_code=status.HTTP_201_CREATED)
+async def create_genre(user: user_dependency, db: db_dependency, genre_request: NewGenreRequest):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    genre_model = Author(**genre_request.model_dump())
+
+    db.add(genre_model)
+    db.commit()
+
+
+@router.post("/add_author", status_code=status.HTTP_201_CREATED)
+async def create_author(user: user_dependency, db: db_dependency, author_request: NewAuthorRequest):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    author_model = Author(**author_request.model_dump())
+
+    db.add(author_model)
+    db.commit()
+
+
+@router.post("/add_book", status_code=status.HTTP_201_CREATED)
+async def create_book(user: user_dependency, db: db_dependency, book_request: NewBookRequest):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    book_model = Books(**book_request.model_dump())
+
+    db.add(book_model)
+    db.commit()
+
+
+@router.delete("/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user_by_id(user: user_dependency, db: db_dependency, user_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user_id).first()
+    user_library_model = db.query(Own_library).filter(Own_library.owner_id == user_id).all()
+
+    if user_model is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    
+    for entry in user_library_model:
+        db.delete(entry)
+
+    db.delete(user_model)
+    db.commit()
+
+
+@router.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(user: user_dependency, db: db_dependency, book_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    book_model = db.query(Books).filter(Books.id == book_id).first()
+    if book_model is None:
+        raise HTTPException(status_code=404, detail='Book not found')
+    
+    db.query(Books).filter(Books.id == book_id).delete()
+    db.commit()
+
--- routers/users.py
+++ routers/users.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from typing import Annotated
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from .auth import get_current_user
+from .auth import get_current_user, validate_passwd
 from pydantic import BaseModel, Field
-from models import Own_library, Books, Author
+from models import Own_library, Books, Author, Users
 from starlette import status
+from passlib.context import CryptContext
 
 
 router = APIRouter(
@@ -31,8 +32,21 @@ class AddBookRequest(UpdateBookRequest):
     book_id: int = Field(gt=0)
 
 
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return db.query(Users).filter(Users.id == user.get('id')).first()
 
 
 @router.get("/my_books/", status_code=status.HTTP_200_OK)
@@ -106,4 +120,25 @@ async def delete_my_book(user: user_dependency, db: db_dependency, my_book_id: i
         raise HTTPException(status_code=404, detail='Book not found')
     db.query(Own_library).filter(Own_library.id == my_book_id).filter(Own_library.owner_id == user.get('id')).delete()
 
+    db.commit()
+
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    if not validate_passwd(user_verification.new_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password must have at least one uppercase, one lowercase, one digit, one symbol, and be at least 6 characters long."
+        )
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password change')
+    
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
     db.commit()
